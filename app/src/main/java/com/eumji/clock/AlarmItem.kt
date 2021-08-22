69e645fe214482191d565d32b21cc7b2cb5b541e
eumji <grapeti200@gmail.com>
2021-08-23 03:24:28 +0900

    Implement Clock-Alarm main page layout

---
+++ app/src/main/java/com/eumji/clock/AlarmItem.kt
@@ -0,0 +1,5 @@
+package com.eumji.clock
+
+class AlarmItem(val time: Int) {
+    
+}
--- app/src/main/java/com/eumji/clock/MainActivity.kt
+++ app/src/main/java/com/eumji/clock/MainActivity.kt
@@ -2,10 +2,22 @@ package com.eumji.clock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val list = ArrayList<AlarmItem>()
+
+        list.add(AlarmItem(1))
+        list.add(AlarmItem(2))
+        list.add(AlarmItem(3))
+        list.add(AlarmItem(4))
+        list.add(AlarmItem(5))
+        list.add(AlarmItem(6))
+        alarm_recyclerview.adapter = AlarmRecyclerAdapter(list)
+        alarm_recyclerview.addItemDecoration(VerticalItemDecorator(20))
     }
+
 }
