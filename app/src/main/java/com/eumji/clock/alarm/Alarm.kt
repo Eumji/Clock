783c5e7bbc184387bd94531ba3b23a097dbf9d4a
eumji <grapeti200@gmail.com>
2021-08-26 19:34:21 +0900

    Implement alarm appbar layout

--- app/src/main/java/com/eumji/clock/alarm/Alarm.kt
+++ app/src/main/java/com/eumji/clock/alarm/Alarm.kt
@@ -5,24 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.eumji.clock.R
 import com.eumji.clock.VerticalItemDecorator
-import com.eumji.clock.alarm.AlarmRecyclerAdapter
-import com.eumji.clock.databinding.ActivityMainBinding
+import com.google.android.material.appbar.AppBarLayout
+import java.lang.IllegalStateException
+import java.lang.Math.abs
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Alarm.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Alarm : Fragment() {
-    // TODO: Rename and change types of parameters
     private lateinit var alarmRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,40 +26,45 @@ class Alarm : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view: View = inflater.inflate(R.layout.fragment_alarm, container, false)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_alarm, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val list = ArrayList<AlarmItem>()
+        val alarmTextView: TextView = view.findViewById(R.id.alarm_appbar_title)
+        val appBarLayout: AppBarLayout = view.findViewById(R.id.alarm_appbar)
+        appBarLayout.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener{_, verticalOffset ->
+            appBarLayout.post{
+                try{
+                    if(abs(verticalOffset)>= appBarLayout.totalScrollRange){
+                        alarmTextView.text="알람"
+                    }
+                    else{
+                        alarmTextView.text=" "
+                    }
+                } catch (ex: IllegalStateException){
+                    println(ex.localizedMessage)
+                }
+            }
+        })
 
-        list.add(AlarmItem(1))
-        list.add(AlarmItem(2))
-        list.add(AlarmItem(3))
-        list.add(AlarmItem(4))
-        list.add(AlarmItem(5))
-        list.add(AlarmItem(6))
+        val list = ArrayList<AlarmItem>()
+        for(i in 1..13){
+            list.add(AlarmItem(i))
+        }
         alarmRecyclerView = view.findViewById(R.id.alarm_recyclerview)
         alarmRecyclerView.adapter = AlarmRecyclerAdapter(list)
         alarmRecyclerView.addItemDecoration(VerticalItemDecorator(20))
+
     }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Alarm.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             Alarm().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
